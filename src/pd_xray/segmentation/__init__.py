f3dc1f2ddec42c320015db2d2821da6a6119bf14
ebzkurt1 <mehmet.bozkurt@worc.ox.ac.uk>
2026-04-23 23:39:43 +0100

    WIP: segmentation of volume code

--- src/pd_xray/segmentation/__init__.py
+++ src/pd_xray/segmentation/__init__.py
@@ -1,6 +1,7 @@
 from pd_xray.segmentation.dataset import LabelledDataset
 from pd_xray.segmentation.features import extract_features, FEATURE_NAMES, N_FEATURES
 from pd_xray.segmentation.random_forest import RFSegmenter
+from pd_xray.segmentation.segment_volume import segment_volume
 
 __all__ = [
     "LabelledDataset",
@@ -8,4 +9,5 @@ __all__ = [
     "FEATURE_NAMES",
     "N_FEATURES",
     "RFSegmenter",
+    "segment_volume",
 ]
