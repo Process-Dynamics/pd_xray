4d083b43762ea73ff287379c944d24aa35a1252e
ebzkurt1 <mehmet.bozkurt@worc.ox.ac.uk>
2026-05-10 00:09:21 +0100

    ADD: sftp backend along with tif image downloading script for multiple file download

--- src/pd_xray/data/__init__.py
+++ src/pd_xray/data/__init__.py
@@ -2,10 +2,12 @@ from pd_xray.data.formats.tiff import TIFFReader
 from pd_xray.data.formats.hdf5 import HDF5Reader
 from pd_xray.data.backends.local import LocalBackend
 from pd_xray.data.backends.s3 import S3Backend
+from pd_xray.data.backends.sftp import SFTPBackend
 
 __all__ = [
     "TIFFReader",
     "HDF5Reader",
     "LocalBackend",
     "S3Backend",
+    "SFTPBackend",
 ]
